d456e54ac587a76de7d25620f537185d8ab47582
YuanZheng Hu <bhu078@uottawa.ca>
2019-11-21 20:39:09 -0500

    Updates

---
+++ precache-manifest.5fbf87a6c0ea15730278ff4d3238723f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f824846e17babbc218cae6dce14dc70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5b598c5aee3028c9637",
+    "url": "/static/js/2.d0173d64.chunk.js"
+  },
+  {
+    "revision": "62c752e2964a04a3a9d2",
+    "url": "/static/js/main.31ebcaa1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ee9bae6e76971f68113c548a7c9b7202",
+    "url": "/static/media/avt.ee9bae6e.jpg"
+  }
+]);
